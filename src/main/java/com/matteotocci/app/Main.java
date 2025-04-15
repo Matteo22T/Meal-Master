1c455a9377941f238b486be86f70202c4225a787
Matteo <matteo.tocci2004@gmail.com>
2025-04-15 10:51:25 +0200

    secodno commit

---
+++ src/main/java/com/matteotocci/app/Main.java
@@ -0,0 +1,10 @@
+package com.matteotocci.app;
+
+import com.matteotocci.app.model.Apertura;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args){
+        Application.launch(Apertura.class,args);
+    }
+}
---
+++ src/main/java/com/matteotocci/app/model/Apertura.java
@@ -0,0 +1,20 @@
+package com.matteotocci.app.model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Apertura extends Application {
+    @Override
+    public void start(Stage stage)throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(Apertura.class.getResource("/com/matteotocci/app/login.fxml"));
+        Scene scene=new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args){launch();}
+}
