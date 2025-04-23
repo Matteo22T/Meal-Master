fb03b3d2a1612e3bd05428fd447c96ebae98e0fa
Matteo <matteo.tocci2004@gmail.com>
2025-04-23 15:46:41 +0200

    Aggiornamento pagina di login e registrazione

--- src/main/java/com/matteotocci/app/controller/login.java
+++ src/main/java/com/matteotocci/app/controller/login.java
@@ -3,9 +3,15 @@ package com.matteotocci.app.controller;
 
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class login {
     @FXML
@@ -60,6 +66,51 @@ public class login {
         active.getStyleClass().add("bottoneAttivo");
 
         inactive.getStyleClass().add("bottoneSpento");
+    }
+
+    @FXML
+    private Button BottoneAccedi;
+    @FXML
+    private Button BottoneRegistrati;
+
+
+    @FXML
+    private void Registrato(ActionEvent event) {
+        try {
+            // Carica il file login.fxml
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/matteotocci/app/PrimaPagina.fxml"));
+            Parent loginRoot = fxmlLoader.load();
+
+            // Crea un nuovo stage (nuova finestra)
+            Stage loginStage = new Stage();
+            loginStage.setScene(new Scene(loginRoot));
+            loginStage.show();
+
+            // SE vuoi chiudere la finestra attuale, togli il commento qui sotto:
+            ((Stage) BottoneRegistrati.getScene().getWindow()).close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void AccessoHomePage(ActionEvent event) {
+        try {
+            // Carica il file login.fxml
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/matteotocci/app/HomePage.fxml"));
+            Parent loginRoot = fxmlLoader.load();
+
+            // Crea un nuovo stage (nuova finestra)
+            Stage loginStage = new Stage();
+            loginStage.setScene(new Scene(loginRoot));
+            loginStage.show();
+
+            // SE vuoi chiudere la finestra attuale, togli il commento qui sotto:
+            ((Stage) BottoneAccedi.getScene().getWindow()).close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
