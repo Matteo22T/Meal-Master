cbf51b024e2436a2565917bf735e15422e33a06d
Matteo <matteo.tocci2004@gmail.com>
2025-04-24 12:30:10 +0200

    Aggiunta tabella alimenti

--- src/main/java/com/matteotocci/app/model/SQLiteConnessione.java
+++ src/main/java/com/matteotocci/app/model/SQLiteConnessione.java
@@ -5,7 +5,7 @@ public class SQLiteConnessione {
     public static Connection connector() {
         try {
             Class.forName("org.sqlite.JDBC");
-            String dbPath = "jdbc:sqlite:C:/Users/Dario De Paola/IdeaProjects/Meal-Master/database.db";
+            String dbPath = "jdbc:sqlite:database.db";
             Connection conn = DriverManager.getConnection(dbPath);
             return conn;
         } catch (Exception e) {
