f3e3e34b17d4b824ff3ab2bc1307b561b6e91d8c
Matteo <matteo.tocci2004@gmail.com>
2025-04-25 12:12:17 +0200

    Aggiornamento pagina HomePage e aggiunta collegamento nel menù

--- src/main/java/com/matteotocci/app/controller/HomePage.java
+++ src/main/java/com/matteotocci/app/controller/HomePage.java
@@ -1,4 +1,29 @@
 package com.matteotocci.app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePage {
+    @FXML
+    private Button BottoneAlimenti;
+    @FXML
+    private void AccessoAlimenti(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/matteotocci/app/Alimenti.fxml"));
+            Parent loginRoot = fxmlLoader.load();
+            Stage loginStage = new Stage();
+            loginStage.setScene(new Scene(loginRoot));
+            loginStage.show();
+            ((Stage) BottoneAlimenti.getScene().getWindow()).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
